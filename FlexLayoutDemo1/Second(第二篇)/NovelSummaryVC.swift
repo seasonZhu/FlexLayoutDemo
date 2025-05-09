24f4ce7a1e158b1b9ad26c66f10c53c37d2b00ce
season <zhujilong1987@163.com>
2025-05-09 16:49:18 +0800

    小说页面布局理解

--- FlexLayoutDemo1/Second(第二篇)/NovelSummaryVC.swift
+++ FlexLayoutDemo1/Second(第二篇)/NovelSummaryVC.swift
@@ -9,9 +9,14 @@
 import UIKit
 import FlexLayout
 
+import RxCocoa
+import NSObject_Rx
+import RxGesture
 
 class NovelSummaryVC: UIViewController {
+    
     fileprivate var rootFlex = UIView()
+    
     fileprivate var summaryView = UIView()
     /// 背景图
     fileprivate let bgImgV = UIImageView(image: UIImage(named: "novel_bg"))
@@ -40,18 +45,36 @@ class NovelSummaryVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         view.backgroundColor = UIColor(white: 0.96, alpha: 1)
         view.addSubview(rootFlex)
         
         coverImgV.image = UIImage(named: "novel_cover")
+        coverImgV.rx.tapGesture().subscribe(onNext:  { [weak self] _ in
+            guard let self else {
+                return
+            }
+            
+            if self.descContainer.isHidden {
+                self.descContainerShow()
+            } else {
+                self.descContainerHidden()
+            }
+            
+            
+        }).disposed(by: rx.disposeBag)
+        
         descLabel.font = UIFont.systemFont(ofSize: 16, weight: .medium)
         descLabel.text = "“玄门羽衣白云心，一琴一剑一丹青”"
         descLabel.textAlignment = .center
         descLabel.textColor = UIColor(0.77, 0.64, 0.48, 1)
         
         configLabel(bookNameLabel, text: "心魔")
+        
         bookNameLabel.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        
         configLabel(authorLabel, text: "沁纸花青")
+        
         levelLabel.text = "Lv4"
         levelLabel.font = UIFont.systemFont(ofSize: 10)
         levelLabel.textColor = UIColor(0, 0.8, 1, 1)
@@ -60,49 +83,69 @@ class NovelSummaryVC: UIViewController {
         levelLabel.layer.borderWidth = 1
         levelLabel.layer.cornerRadius = 4
         levelLabel.layer.masksToBounds = true
+        
         configLabel(readCountLabel, text: "466万人读过")
+        
         configLabel(categoryLabel, text: "仙侠 | 幻想修真")
+        
         configLabel(wordCoundAndStateLabel, text: "266.0万字 | 连载")
         
         
         rootFlex.flex.marginTop(88).define { flex in
+            
             flex.addItem(summaryView).backgroundColor(.white).define { flex in
+                
                 flex.addItem(bgImgV).position(.absolute).left(0).top(0).width(100%).height(100%)
+                
                 // 浅绿色盒子, 横向排列子盒子, 距离父盒子(summaryView)左右外边距为15，顶底各12外边距
                 flex.addItem().direction(.row).marginVertical(12).marginHorizontal(15).define { flex in
+                    
                     flex.addItem().marginRight(22).width(24.6%).aspectRatio(17/23).define { flex in
                         // 封面图, 右外边距22
                         flex.addItem(coverImgV)
+                        
                         flex.addItem(verifiedImgV).position(.absolute).right(-5).bottom(-5).size(20)
                     }
+                    
                     // 浅蓝色盒子
                     flex.addItem().justifyContent(.spaceBetween).grow(1).shrink(1).define { flex in
                         flex.addItem(bookNameLabel)
+                        
                         flex.addItem().direction(.row).define { flex in
                             flex.addItem(authorLabel)
+                            
                             flex.addItem(levelLabel).paddingHorizontal(6).marginLeft(8)
                         }
+                        
                         flex.addItem(readCountLabel)
+                        
                         flex.addItem(categoryLabel)
+                        
                         flex.addItem(wordCoundAndStateLabel)
                     }
                 }
-                // 浅红色盒子, padding(上, 左, 下, 右)
-                flex.addItem(descContainer).padding(12, 10, 8, 10)
-                    .backgroundColor(.black).define { flex in
-                    flex.addItem(descLabel)
-                }
             }
+            
+            /// 把这个盒子移到了外面,这样显示效果才正常 浅红色盒子, padding(上, 左, 下, 右)
+            flex.addItem(descContainer).padding(12, 10, 8, 10)
+                .backgroundColor(.black).define { flex in
+                flex.addItem(descLabel)
+            }
+            
             flex.addItem().grow(1).backgroundColor(.lightGray)
         }
-        
-        // 模拟不需要显示底部浅红色view
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-            self.descContainer.flex.isIncludedInLayout = false
-            self.descContainer.isHidden = true
-            self.view.setNeedsLayout()
-            self.view.layoutIfNeeded()
-        }
+    }
+    
+    private func descContainerHidden() {
+        descContainer.flex.isIncludedInLayout = false
+        descContainer.isHidden = true
+        view.setNeedsLayout()
+    }
+    
+    private func descContainerShow() {
+        descContainer.flex.isIncludedInLayout = true
+        descContainer.isHidden = false
+        view.setNeedsLayout()
     }
     
     override func viewDidLayoutSubviews() {
